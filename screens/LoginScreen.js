2928e8cd77d4335a4f4843e13fac601c68d4ff9d
quyenpham00 <85977597+quyenpham00@users.noreply.github.com>
2023-03-03 23:26:20 +0700

    Initial Commit

---
+++ screens/LoginScreen.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import useAuth from '../hooks/useAuth'
+
+const LoginScreen = () => {
+  const { user } = useAuth()
+
+  console.log(user)
+
+  return (
+    <View>
+      <Text>LoginScreen</Text>
+    </View>
+  )
+}
+
+export default LoginScreen
